169ae6a1683d5e11194b7f37557fe06006e68d53
Josue St Jean <jojeansaint@gmail.com>
2019-03-12 18:36:19 -0400

    Implementing function for a button....
    Signed-off-by: Josue St Jean <jojeansaint@gmail.com>

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.atry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+    //change name
+    public void changeName(View view){
+        tvName = findViewById(R.id.tvName);
+        tvName.setText("Josue");
+
+    }
+
 }
